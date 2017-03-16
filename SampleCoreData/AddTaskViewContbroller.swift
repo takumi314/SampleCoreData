ef9ff9aca512b7d014d2ec31cec426f378165638
takumi314 <isospin_symmetry@me.com>
2017-03-16 21:46:35 +0900

    todo

--- SampleCoreData/AddTaskViewContbroller.swift
+++ SampleCoreData/AddTaskViewContbroller.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+class AddTaskViewContbroller: UIViewController {
+
+    @IBOutlet weak var addTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func addTextOnButton(_ sender: UIButton) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let task = Task(context: context) // Link Task & Context
+        task.name = addTextField.text!
+
+        // Save the data to coredata
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+
+        let _ = navigationController?.popViewController(animated: true)
+    }
+
+}
--- SampleCoreData/ViewController.swift
+++ SampleCoreData/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var tasks = [Task]()
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getData() {
+        do {
+            tasks = try context.fetch(Task.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+
+        if let myName = task.name {
+            cell.textLabel?.text = myName
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+
+            do {
+                tasks = try context.fetch(Task.fetchRequest())
+            } catch {
+                print("Fetching Failed")
+            }
+        }
+        tableView.reloadData()
+    }
+
+}
